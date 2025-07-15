7ec964d45ffd78e22d2d796842a6b2ac7b1f3d4a
Oli <oli.cste10@gmail.com>
2025-07-15 16:55:12 +0600

    Fix build issues and vulnerabilities

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,20 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: 'dist',
+    emptyOutDir: true,
+    sourcemap: false,
+    chunkSizeWarningLimit: 1600,
+  },
+  server: {
+    port: 3000,
+    strictPort: true,
+  },
+  preview: {
+    port: 3000,
+    strictPort: true,
+  }
 })
